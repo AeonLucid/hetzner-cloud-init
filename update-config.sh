8b0477cc7aa59068f4763778417a94b67ff241d1
AeonLucid <aeonlucid@gmail.com>
2022-07-03 01:49:58 +0200

    Update update-config.sh

--- update-config.sh
+++ update-config.sh
@@ -15,10 +15,6 @@ case $key in
     shift
     shift
   ;;
-  --floating-ips)
-    FLOATING_IPS="1"
-    shift
-  ;;
   *)
     shift
   ;;
@@ -54,20 +50,3 @@ declare -p REMOVED
 for IP in "${REMOVED[@]}"; do
   ufw deny from "$IP"
 done
-
-FLOATING_IPS=${FLOATING_IPS:-"0"}
-
-if [ "$FLOATING_IPS" == "1" ]; then
-  FLOATING_IPS=( $(curl -H 'Accept: application/json' -H "Authorization: Bearer ${TOKEN}" 'https://api.hetzner.cloud/v1/floating_ips' | jq -r '.floating_ips[].ip') )    
-
-  for IP in "${FLOATING_IPS[@]}"; do
-    ip addr add $IP/32 dev eth0
-  done  
-fi
-
-# Cloudflare
-curl -s https://www.cloudflare.com/ips-v4 -o /tmp/cf_ips
-curl -s https://www.cloudflare.com/ips-v6 >> /tmp/cf_ips
-
-for cfip in `cat /tmp/cf_ips`; do /usr/sbin/ufw allow proto tcp from $cfip to any port 80 comment 'Cloudflare IP'; done
-for cfip in `cat /tmp/cf_ips`; do /usr/sbin/ufw allow proto tcp from $cfip to any port 443 comment 'Cloudflare IP'; done
